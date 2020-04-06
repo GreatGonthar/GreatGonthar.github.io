eaaf4de3c5b2fbe745fe8c5ca4264d312d4c7e2b
Gonthar <gontharforeverrules@gmail.com>
2020-04-06 17:12:54 +1000

    добавили функцию смерти

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let ctx = myCanvas.getContext("2d");
 let snakeX = 0;
 let snakeY = 300;
 let cleaner = [];
-let snakeLong = 10;
+let snakeLong = 20;
 
 
 myCanvas.width = 800;
@@ -17,6 +17,7 @@ let directionX = 1;
 let directionY = 0;
 
 function drawPlayer(){
+	death();
 	ctx.fillStyle = 'green';
 	ctx.fillRect(snakeX, snakeY, 20, 20);
 	if (cleaner.length < snakeLong){
@@ -24,25 +25,36 @@ function drawPlayer(){
 	}
 	snakeX += 20 * directionX;
 	snakeY += 20 * directionY;
-	if (cleaner.length >= snakeLong){
-		ctx.fillStyle = 'black';
-		ctx.fillRect(cleaner[0][0], cleaner[0][1], 20, 20);
-		//ctx.fillRect(200,300, 20, 20);
-		console.log(cleaner[0])
-		cleaner.shift();
-
-	}
-
-	ctx.fillStyle = 'black';
-	
-	//ctx.fillRect(appleX - (snakeLongX*alligmentX), appleY - (snakeLongY*alligmentY), 20, 20);
-
 
 	(snakeX > myCanvas.width) ? snakeX = 0: true;
 	(snakeX < 0) ? snakeX = myCanvas.width: true;
 	(snakeY > myCanvas.height) ? snakeY = 0: true;
 	(snakeY < 0) ? snakeY = myCanvas.height: true;
+	drawCleaner();
+	
 }
+
+function drawCleaner(){	
+	if (cleaner.length >= snakeLong){
+		ctx.fillStyle = 'black';
+		ctx.fillRect(cleaner[0][0], cleaner[0][1], 20, 20);				
+		cleaner.shift();
+		
+	}
+}
+function death(){
+	for (let body of cleaner) {
+		if (snakeX == body[0] && snakeY == body[1]){
+  			alert('конец');
+  			ctx.fillStyle = 'black';
+			ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
+			snakeX = 0;
+			snakeY = 300;
+			directionX = 1;
+			directionY = 0;
+  		}
+	}
+}	
 
 function keyMovePlayer(e){	
 	switch (e.keyCode) {
@@ -68,9 +80,7 @@ function keyMovePlayer(e){
 			break;	
 		case 32:
 			console.log(cleaner.length);
-
-	}	
-	//drawPlayer();		
+	}		
 }
 addEventListener("keydown", keyMovePlayer);
 
