cc0ceead494655cbd44d77df9a63fd1d4102211d
Gonthar <gontharforeverrules@gmail.com>
2020-04-11 15:02:18 +1000

    сделал новую версию Мастером

--- app.js
+++ app.js
@@ -3,13 +3,12 @@
 
 let myCanvas = document.getElementById("my_canvas");
 let ctx = myCanvas.getContext("2d");
-let snakeX = 0;
-let snakeY = 300;
-let cleaner = [];
-let snakeLong = 5;
 let appleX;
 let appleY;
 let numbersApple = 0;
+let snakeLong = [[0,300],[20,300]];
+let snakeX ;
+let snakeY ;
 
 
 myCanvas.width = 800;
@@ -20,67 +19,55 @@ let directionX = 1;
 let directionY = 0;
 
 function mainLoop(){	
-	death();
-		apple();	
+		
 	drawPlayer();
-	drawCleaner();
+	apple();
+	death();
 
 }
 
 function drawPlayer(){
-	ctx.fillStyle = 'green';
-	ctx.fillRect(snakeX, snakeY, 20, 20);
-	if (cleaner.length < snakeLong){
-		cleaner.push([snakeX, snakeY]);		
-	}
-	snakeX += 20 * directionX;
-	snakeY += 20 * directionY;
+	snakeX = snakeLong[snakeLong.length-1][0];
+	snakeY = snakeLong[snakeLong.length-1][1];
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);	
+	snakeLong.push([snakeX+(20 * directionX), snakeY+(20 * directionY)]);	
 
-	(snakeX > myCanvas.width) ? snakeX = 0: true;
-	(snakeX < 0) ? snakeX = myCanvas.width: true;
-	(snakeY > myCanvas.height) ? snakeY = 0: true;
-	(snakeY < 0) ? snakeY = myCanvas.height: true;		
-}
+	if (snakeX != appleX || snakeY != appleY){snakeLong.shift()};	
 
-function drawCleaner(){	
-	if (cleaner.length >= snakeLong){
-		ctx.fillStyle = 'black';
-		ctx.fillRect(cleaner[0][0], cleaner[0][1], 20, 20);				
-		cleaner.shift();		
-	}
+		for (let i = 0; i < (snakeLong.length-1); i++){
+			ctx.fillStyle = 'green';
+			ctx.fillRect(snakeLong[i][0], snakeLong[i][1], 20, 20);	
+		}
+
+	(snakeX > myCanvas.width) ? snakeLong[snakeLong.length-1][0] = 0: true;
+	(snakeX < 0) ? snakeLong[snakeLong.length-1][0] = myCanvas.width: true;
+	(snakeY > myCanvas.height) ? snakeLong[snakeLong.length-1][1] = 0: true;
+	(snakeY < 0) ? snakeLong[snakeLong.length-1][1] = myCanvas.height: true;	
 }
 
 function death(){
-	for (let body of cleaner) {
-		if (snakeX == body[0] && snakeY == body[1]){
-  			alert('конец');
-  			ctx.fillStyle = 'black';
-			ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
-			snakeX = 0;
-			snakeY = 300;
+	for (let i = 0; i < (snakeLong.length-1); i++){
+		console.log(snakeLong[0][0])
+		if (snakeLong[(snakeLong.length-1)][0] == snakeLong[i][0] && snakeLong[(snakeLong.length-1)][1] == snakeLong[i][1]){			
+			alert('заново')
+			snakeLong = [[0,300],[20,300]]
 			directionX = 1;
 			directionY = 0;
-  		}
+		}
 	}
-}	
+}
 
 function apple(){
-	if (numbersApple < 3){
-	appleX = Math.floor(Math.random()*(myCanvas.width/20))*20;
-	appleY = Math.floor(Math.random()*(myCanvas.height/20))*20;
+	if (snakeX == appleX && snakeY == appleY){numbersApple = 0;}	
+
+	if (numbersApple < 1){
+		appleX = Math.floor(Math.random()*(myCanvas.width/20))*20;
+		appleY = Math.floor(Math.random()*(myCanvas.height/20))*20;	
+		numbersApple += 1;
+	}
 	ctx.fillStyle = 'red';
-	ctx.fillRect(appleX, appleY, 20, 20);	
-	numbersApple += 1;
-	}
-
-	if (snakeX == appleX && snakeY == appleY){
-	console.log(appleX, snakeX)
-	numbersApple = 0;
-	snakeLong++;
-	}
-	
-	//console.log(appleX, snakeX)
-
+	ctx.fillRect(appleX, appleY, 20, 20);
 }
 
 function keyMovePlayer(e){	
@@ -105,8 +92,6 @@ function keyMovePlayer(e){
 			(directionY != -1) ? directionY = 1: true;
 			directionX = 0;
 			break;	
-		case 32:
-			console.log(cleaner.length);
 	}		
 }
 addEventListener("keydown", keyMovePlayer);
